ea18b97dfecf1afd6fc2b17d5e3a3b799b987ba8
Jaheda Sultana <jahedasultana47@gmail.com>
2025-06-30 22:15:35 +0600

    added FUNCTION cURRYING

--- test.js
+++ test.js
@@ -46,17 +46,46 @@
 
 
 
-import {produce} from "immer";
-const employee = {
-    name: "Mir",
-    address: {country: "Bangladesh", city: "Dhaka"},
+// import {produce} from "immer";
+// const employee = {
+//     name: "Mir",
+//     address: {country: "Bangladesh", city: "Dhaka"},
+// };
+
+// const employee2 = produce(employee, (draft) =>{
+    
+//     draft.name = "Mezba";
+//     draft.address.city = "Chittagong";
+// }) ;
+// console.log(employee == employee2);
+// console.log(employee);
+// console.log(employee2);
+
+
+
+
+// * Function Currying
+
+// const add = (a) => (b) => a + b;
+
+
+function add(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        };
+    };
 };
 
-const employee2 = produce(employee, (draft) =>{
-    
-    draft.name = "Mezba";
-    draft.address.city = "Chittagong";
-}) ;
-console.log(employee == employee2);
-console.log(employee);
-console.log(employee2);
+
+console.log(add(2));
+
+
+
+ const totalPrice = (discount) => (amount) => amount - amount * discount;
+
+ const withDiscount = totalPrice(0.3);
+
+ console.log(withDiscount(100));
+ console.log(withDiscount(300));
+ console.log(withDiscount(400));
