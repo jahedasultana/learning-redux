2725d77ca9be90e3603c8ea195f78e5362d60b63
Jaheda Sultana <jahedasultana47@gmail.com>
2025-07-01 01:32:42 -0600

    navbar added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,19 @@
 import Logo from "@/assets/Logo";
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggle";
 
 const Navbar = () => {
     return (
-        <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">  
+        <nav className="max-w-7xl mx-auto h-16 flex justify-between items-center gap-3 px-5">  
             
         <div className="flex items-center">
          <Logo></Logo><span className="font-bold ml-2">Tesk</span>Master
         </div>
         <Link to="/users">Users</Link>
         <Link to="/tasks">Tasks</Link>
+        <div className="ml-auto">
+            <ModeToggle/>
+        </div>
         </nav>
 
     );
